a47d85829120a7c005b248776774a3fcc5c8e9d4
Filip Bártek <filip.bartek@hotmail.com>
2014-05-06 00:44:07 +0200

    slnkcctr.cpp: Reformatted

--- slnkcctr/slnkcctr/slnkcctr.cpp
+++ slnkcctr/slnkcctr/slnkcctr.cpp
@@ -1,11 +1,5 @@
-// slnkcctr.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	return 0;
 }
-
