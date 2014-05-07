0a7787d8f8114fc3cbf80b67ffa6cc07e8e06d42
Filip Bártek <filip.bartek@hotmail.com>
2014-05-07 02:33:08 +0200

    Added DetectorColor class with dummy implementation

---
+++ slnkcctr/slnkcctr/DetectorColor.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+#include "DetectorColor.hpp"
+
+FrameAnnotation
+DetectorColor::detect(const cv::Mat& mat) {
+	// TODO: Implement.
+	return FrameAnnotation();
+}
---
+++ slnkcctr/slnkcctr/DetectorColor.hpp
@@ -0,0 +1,15 @@
+// http://opencv-srf.blogspot.com/2010/09/object-detection-using-color-seperation.html
+
+#pragma once
+
+#include "stdafx.h"
+
+// cv::
+#include <opencv2/core/core.hpp> // Mat
+
+#include "FrameAnnotation.hpp" // FrameAnnotation
+
+class DetectorColor {
+public:
+	FrameAnnotation detect(const cv::Mat& mat);
+};
