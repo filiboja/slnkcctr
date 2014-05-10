cda94573f983deae331c9aefef098acf45d570d3
Filip Bártek <filip.bartek@hotmail.com>
2014-05-10 23:18:01 +0200

    slnkcctr.cpp: Lots of various changes; now more or less clean

--- slnkcctr/slnkcctr/slnkcctr.cpp
+++ slnkcctr/slnkcctr/slnkcctr.cpp
@@ -1,4 +1,3 @@
-// Displays video stream from first camera
 // Exit by pressing Escape
 
 #include "stdafx.h"
@@ -6,6 +5,7 @@
 // std::
 #include <iostream> // cout, endl
 #include <iomanip> // setiosflags, setprecision
+#include <string> // string
 
 // cv::
 #include <opencv2/core/core.hpp> // Mat
@@ -16,53 +16,101 @@
 
 int main(int argc, char *argv[]) {
 	// Configuration
-	const int delay = 10;
-	const char * const WIN_MAIN = "slnkcctr";
+	const char * const WIN_ORIGINAL = "slnkcctr";
 	const char * const WIN_DETECTOR = "Detector";
 	const char KEY_ESC = (char)27;
 	double frameWidth = 640.0;
 	double frameHeight = 480.0;
+	enum CapMode { CAP_MODE_DEVICE, CAP_MODE_FILE };
+	CapMode capMode = CAP_MODE_FILE;
+	const int capDevice = 0;
+	const std::string capFilename = "C:\\Users\\TTT\\Videos\\Logitech Webcam\\Slinky night.wmv";
+	const bool output = false;
+	const std::string outputFilename = "out.avi";
+	const int defaultFps = 15; // in case FPS is not provided by `cap`
+	const bool showOriginal = true;
 
 	// Format `double` output
 	std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(3);
 
 	// Print instructions
+	std::cout << "slnkcctr" << std::endl;
 	std::cout << "Press Escape to exit." << std::endl;
 
-	// Open video stream
-	cv::VideoCapture video(0);
-	if (!video.isOpened()) {
-		std::cout << "Could not open video stream." << std::endl;
+	// Open cap stream
+	cv::VideoCapture cap;
+	switch (capMode) {
+	case CAP_MODE_DEVICE:
+		std::cout << "Capture mode: device" << std::endl;
+		std::cout << "Capture device: " << capDevice << std::endl;
+		cap = cv::VideoCapture(capDevice);
+		break;
+	case CAP_MODE_FILE:
+		std::cout << "Capture mode: file" << std::endl;
+		std::cout << "Capture filename: " << capFilename << std::endl;
+		cap = cv::VideoCapture(capFilename);
+		break;
+	}
+	if (!cap.isOpened()) {
+		std::cout << "Could not open capture." << std::endl;
+		std::cout << "Exiting with code 1." << std::endl;
 		return 1;
 	}
 
-	CaptureProperties capProps;
-
 	// Set
-	std::cout << "Set frame width: " << video.set(CV_CAP_PROP_FRAME_WIDTH, frameWidth) << std::endl;
-	std::cout << "Set frame height: " << video.set(CV_CAP_PROP_FRAME_HEIGHT, frameHeight) << std::endl;
+	std::cout << "Set frame width: " << cap.set(CV_CAP_PROP_FRAME_WIDTH, frameWidth) << std::endl;
+	std::cout << "Set frame height: " << cap.set(CV_CAP_PROP_FRAME_HEIGHT, frameHeight) << std::endl;
 
-	// Print camera info
-	capProps.print(video);
+	// Print capture info
+	CaptureProperties capProps;
+	std::cout << "Capture properties:" << std::endl;
+	capProps.print(cap);
 	
 	// Open window
-	cv::namedWindow(WIN_MAIN, CV_WINDOW_AUTOSIZE);
+	if (showOriginal) {
+		cv::namedWindow(WIN_ORIGINAL, CV_WINDOW_AUTOSIZE);
+	}
+
+	int fps = cap.get(CV_CAP_PROP_FPS);
+	if (fps <= 0) {
+		fps = defaultFps;
+	}
+	int delay = 1000 / fps;
+	std::cout << "Desired FPS: " << fps << std::endl;
+	std::cout << "Frame delay: " << delay << std::endl;
+	std::cout << "Actual FPS: " << 1000.0 / delay << std::endl;
+
+	// Open output video
+	cv::Size size((int) cap.get(CV_CAP_PROP_FRAME_WIDTH), (int) cap.get(CV_CAP_PROP_FRAME_HEIGHT));
+	cv::VideoWriter outputVideo;
+	if (output) {
+		outputVideo.open(outputFilename, -1, fps, size, true);
+		if (!outputVideo.isOpened()) {
+			std::cout << "Could not open output video stream." << std::endl;
+		}
+	}
 
 	DetectorColor detector(WIN_DETECTOR);
 
-	char c = 0;
 	// Main loop
-	while (c != KEY_ESC) {
+	int key = 0;
+	assert(key != KEY_ESC);
+	while (key != KEY_ESC) {
 		cv::Mat frame;
-		video >> frame;
+		cap >> frame;
 		if (frame.empty()) {
 			std::cout << "Empty frame." << std::endl;
+			break;
 		}
 		FrameAnnotation annotation = detector.detect(frame);
-		std::cout << annotation << std::endl;
-		annotation.draw(frame);
-		cv::imshow(WIN_MAIN, frame);
-		c = (char)cvWaitKey(delay);
+		if (showOriginal) {
+			annotation.draw(frame);
+			cv::imshow(WIN_ORIGINAL, frame);
+		}
+		if (output && outputVideo.isOpened()) {
+			outputVideo << frame;
+		}
+		key = cv::waitKey(delay);
 	}
 
 	return 0;
