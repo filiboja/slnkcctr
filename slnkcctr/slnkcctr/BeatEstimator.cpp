2de8a321fef4f2bf798fc34e76e30cf0e615028c
Filip Bártek <filip.bartek@hotmail.com>
2014-06-02 06:06:23 +0200

    BeatEstimator.cpp: Removed obsolete commented out code

--- slnkcctr/slnkcctr/BeatEstimator.cpp
+++ slnkcctr/slnkcctr/BeatEstimator.cpp
@@ -4,18 +4,6 @@
 
 #include "BeatEstimator.h"
 
-/*bool BeatEstimator::estimate(clock_t clock, Pos pos) {
-	bool result = false;
-	double y = pos.y;
-	if (y >= ballY + 50) {
-		// Beat!
-		result = true;
-		ballY = y;
-	}
-	ballY = (ballY * 0.7) + (y * 0.3);
-	return result;
-}*/
-
 BeatEstimator::BeatEstimator(const clock_t& clock)
 	: estimator0(clock, BallBeatEstimator::Color(0, 0, 255)), estimator1(clock, BallBeatEstimator::Color(0, 255, 0))
 {}
